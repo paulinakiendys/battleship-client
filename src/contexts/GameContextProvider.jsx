5032a6b76de5b8ae5b225a12bdb73415df67de61
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-05-14 15:43:11 +0200

    Established socket.io connection

---
+++ src/contexts/GameContextProvider.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext } from 'react'
+import socketio from "socket.io-client";
+
+const GameContext = createContext()
+const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL);
+
+export const useGameContext = () => {
+	return useContext(GameContext)
+}
+
+const GameContextProvider = ({ children }) => {
+
+	const values = {
+		socket,
+	}
+
+	return (
+		<GameContext.Provider value={values}>
+			{children}
+		</GameContext.Provider>
+	)
+}
+
+export default GameContextProvider
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,8 @@
+import { useGameContext } from "../contexts/GameContextProvider"
+
 const HomePage = () => {
+	const { socket } = useGameContext()
+
 	return (
 		<>
 			<h1>Welcome to Battleship!</h1>
