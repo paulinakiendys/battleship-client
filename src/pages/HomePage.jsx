e96f8545472d4b3dd156e9201d5c73b3abd4ed16
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-05-15 10:41:36 +0200

    Added start game button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
+import { Button } from 'react-bootstrap'
 const HomePage = () => {
+	const handleStartGameClick = () => {
+		// Add code here
+	}
 	return (
 		<>
 			<h1>Welcome to Battleship!</h1>
+			<Button onClick={handleStartGameClick}>Start Game</Button>
 		</>
 	)
 }
