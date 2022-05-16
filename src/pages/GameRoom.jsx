a99eaae6ad999cbc32f1ad86da234c3e2aba793b
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-05-16 18:42:31 +0200

    Display connected users

--- src/pages/GameRoom.jsx
+++ src/pages/GameRoom.jsx
@@ -1,22 +1,45 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useGameContext } from '../contexts/GameContextProvider'
 
 const GameRoom = () => {
     const { room_id } = useParams()
     const { socket } = useGameContext()
+    const [users, setUsers] = useState([])
+
+    const handleUpdateUsers = userlist => {
+        console.log("Got new userlist", userlist)
+        setUsers(userlist)
+    }
 
     // connect to room when component is mounted
+
+    /**
+     * @todo fix bug: join request emitted twice
+     */
     useEffect(() => {
         // emit join request
         socket.emit('user:joined', room_id, status => {
             console.log(`Successfully joined ${room_id}`, status)
+            
         })
+
+        // listen for updated userlist
+        socket.on('user:list', handleUpdateUsers)
+
     }, [socket, room_id])
 
     return (
         <>
             <h1>Game room: {room_id}</h1>
+            <div id="users">
+                <h2>Users</h2>
+                <ul id="online-users">
+                    {Object.values(users).map((user, index) =>
+                        <li key={index}>{user} connected</li>
+                    )}
+                </ul>
+            </div>
         </>
     )
 }
