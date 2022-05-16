354b4075ba45ed882f6727a631d73e0608a243e3
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-05-16 15:07:16 +0200

    Handle user join

--- src/pages/GameRoom.jsx
+++ src/pages/GameRoom.jsx
@@ -1,11 +1,24 @@
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { useGameContext } from '../contexts/GameContextProvider'
+
 const GameRoom = () => {
     const { room_id } = useParams()
-	return (
-		<>
-			<h1>Game room: {room_id}</h1>
-		</>
-	)
+    const { socket } = useGameContext()
+
+    // connect to room when component is mounted
+    useEffect(() => {
+        // emit join request
+        socket.emit('user:joined', room_id, status => {
+            console.log(`Successfully joined ${room_id}`, status)
+        })
+    }, [socket, room_id])
+
+    return (
+        <>
+            <h1>Game room: {room_id}</h1>
+        </>
+    )
 }
 
 export default GameRoom
