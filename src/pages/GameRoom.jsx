a2c06be9d58cb549c13a49db4e6470eca214f0e1
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-05-18 13:54:20 +0200

    Display message when opponent disconnects

--- src/pages/GameRoom.jsx
+++ src/pages/GameRoom.jsx
@@ -1,11 +1,23 @@
 import { useParams } from 'react-router-dom'
+import { useGameContext } from '../contexts/GameContextProvider'
+import { useEffect, useState } from 'react'
 
 const GameRoom = () => {
     const { room_id } = useParams()
+    const [opponentDisconnected, setOpponentDisconnected] = useState(false)
+    const { socket } = useGameContext()
+
+    useEffect(() => {
+        // listen for when a user disconnects
+        socket.on('user:disconnected', () => {
+            setOpponentDisconnected(true)
+        })
+    }, [socket])
 
     return (
         <>
             <h1>Game room: {room_id}</h1>
+            {opponentDisconnected && (<p>Your opponent left the battle 😥</p>)}
         </>
     )
 }
