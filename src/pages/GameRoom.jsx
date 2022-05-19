6a92a25168fa21c595c6991194f5133447c0950d
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-05-19 14:21:36 +0200

    Added basic mockup to experiment with

--- src/pages/GameRoom.jsx
+++ src/pages/GameRoom.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { useGameContext } from '../contexts/GameContextProvider'
 import { useEffect, useState } from 'react'
+import { Button, Form, InputGroup } from 'react-bootstrap'
 
 const GameRoom = () => {
     const { room_id } = useParams()
@@ -16,8 +17,44 @@ const GameRoom = () => {
 
     return (
         <>
-            <h1>Game room: {room_id}</h1>
+            <h1>{room_id}</h1>
             {opponentDisconnected && (<p>Your opponent left the battle 😥</p>)}
+            <div id="game-wrapper">
+                <div id="board-wrapper">
+                    <h2>Your Battleships</h2>
+                    <h3>Ships remaining: <span>3</span></h3>
+                    <div id="board">
+                        {/* Here is user's board */}
+                    </div>
+                </div>
+                <div id="chat-wrapper">
+                    <div id="chat">
+                        {/* Here is log/chat */}
+                        <ul id="messages">
+                            <li>Blabla</li>
+                            <li>Blabla</li>
+                            <li>Blabla</li>
+                        </ul>
+                    </div>
+                    <Form>
+                        <InputGroup>
+                            <Form.Control type='text' />
+                            <Button type='submit'>Send</Button>
+                        </InputGroup>
+                    </Form>
+                    <div id="buttons-wrapper">
+                        <Button variant='warning'>Change</Button>
+                        <Button variant='success'>Ready</Button>
+                    </div>
+                </div>
+                <div id="board-wrapper">
+                    <h2>Opponent's Battleships</h2>
+                    <h3>Ships remaining: <span>4</span></h3>
+                    <div id="board">
+                        {/* Here is opponent's board */}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
