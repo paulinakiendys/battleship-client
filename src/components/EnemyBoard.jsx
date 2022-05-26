0d4e2e06c4ba010d86bb4344ef84104bfe281cf2
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-05-26 20:48:49 +0200

    Updated board view for user and enemy

--- src/components/EnemyBoard.jsx
+++ src/components/EnemyBoard.jsx
@@ -3,32 +3,53 @@ import { useParams } from 'react-router-dom'
 
 let shotFired;
 
-export default function EnemyBoard(props) {
+export default function EnemyBoard({ owner, title, check }) {
   const { room_id } = useParams()
   const { gameUsername, socket } = useGameContext()
 
   const board = {
-    "rows": [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+    "rows": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     "cols": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
   }
 
   console.log("ROOM ID:", room_id)
 
-    // Send ID of the cell the user clicked on to fire
-  
-   
-    return (
+  // Send ID of the cell the user clicked on to fire
+
+  return (
+    <>
       <table id="enemyTable">
-        <caption className="table-title">{props.title}</caption>
-            <tbody>
-            {board.rows.map(row => (
-              <tr key={row}>
-                {board.rows.map(col => (
-                  <td className={props.owner} onClick={props.check} id={board.cols[col] + board.rows[row]} key={board.rows[row] + board.cols[col]}>{board.rows[row] + board.cols[col]}</td>
-                ))}
-              </tr>
+        <caption className="table-title">{title}</caption>
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            {board.cols.map((letter, index) => (
+              <th
+                key={index}
+                scope="col">
+                {letter}
+              </th>
             ))}
-          </tbody>
+          </tr>
+        </thead>
+        <tbody>
+          {board.rows.map((number, index) => (
+            <tr key={index}>
+              <th scope="row">{number}</th>
+              {board.cols.map((letter, index) => (
+                <td
+                  key={index}
+                  id={number + letter}
+                  className={owner}
+                  onClick={check}
+                >
+                  {/* {letter + number} */}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </table>
-    )
+    </>
+  )
 }
